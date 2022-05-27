4897924a4dcfad6b493bc4fbaf61b0c07a862603
Budi Darmawan <vbudi@us.ibm.com>
2022-05-27 08:02:54 -0500

    mod secret
    Signed-off-by: Budi Darmawan <vbudi@us.ibm.com>

--- sko-sample/apic-cust/apic-user-mail-secret.sh
+++ sko-sample/apic-cust/apic-user-mail-secret.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 oc create secret -n tools generic apic-setup-mail \
-  --from-literal MAILTRAP-USER=${MAILTRAP-USER} --from-literal MAILTRAP-PWD=${MAILTRAP_PWD} \
-  -o yaml --dry-run=client
+  --from-literal MAILTRAP_USER=${MAILTRAP_USER} --from-literal MAILTRAP_PWD=${MAILTRAP_PWD} \
+  -o yaml --dry-run=client > apic-setup-mail-user-secret.yaml
